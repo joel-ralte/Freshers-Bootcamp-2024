2ec9ada92ceaa2e5ffa5d0462ae04eda90db22a1
Joel Ralte <109339233+joel-ralte@users.noreply.github.com>
2024-01-09 15:34:33 +0530

    Update Filter Program.java

--- Filter Program.java
+++ Filter Program.java
@@ -3,31 +3,29 @@ import java.util.function.Predicate;
 
 class FilterStringArray {
     public static void main(String[] args) {
-        String[] array = { "ask","play","type","code","asia","app" };
-        ArrayList<String> filtered = new ArrayList<>();
+        // Variable declaration and input
+        String[] inputArray = { "ask","play","type","code","asia","app" };
+        ArrayList<String> filteredArray = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         
         System.out.print("Enter the starting string to search: ");
-        String s = sc.nextLine();
+        String inputString = sc.nextLine();
+        Predicate<String> startsWith = str -> str.startsWith(inputString);
         
-        filtered = filter(array,s);
-        display(filtered);
+        // Function calls
+        filteredArray = filter(inputArray,startsWith);
+        displayInTerminal(filteredArray);
     }
-    static ArrayList filter(String[] array, String s){
-        Predicate<String> startsWith = str -> str.startsWith(s);
-        ArrayList<String> filtered = new ArrayList<>();
-        for(int i=0;i<array.length;i++){
-              if(startsWith.test(array[i])){
-                  filtered.add(array[i]);
+    static ArrayList filter(String[] inputArray, Predicate<String> startsWith){
+        ArrayList<String> filteredArray = new ArrayList<>();
+        for(int i=0;i<inputArray.length;i++){
+              if(startsWith.test(inputArray[i])){
+                  filteredArray.add(inputArray[i]);
               }
         }
-        return filtered;
+        return filteredArray;
     }
-    static void display(ArrayList<String> list){
-        if(list.isEmpty()){
-            System.out.println("No matches");
-            return;
-        }
+    static void displayInTerminal(ArrayList<String> list){
         for(int i=0;i<list.size();i++)
             System.out.println(list.get(i));
     }
