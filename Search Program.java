05931e10f09b53413f298f511a82c4fd49352c18
Joel Ralte <109339233+joel-ralte@users.noreply.github.com>
2024-01-09 13:52:23 +0530

    Update Search Program.java

--- Search Program.java
+++ Search Program.java
@@ -4,7 +4,7 @@ class SearchString {
     public static void main(String[] args) {
         String[] array = { "ask","play","type","code" };
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the string to search: ");
+        System.out.print("Enter the starting string to search: ");
         String s = sc.nextLine();
         boolean ans = search(array,s);
         if(ans){
