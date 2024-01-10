6f5df9c9ca7efb7e0d2d1793f0f35a1bfa87dffd
Joel Ralte <109339233+joel-ralte@users.noreply.github.com>
2024-01-10 10:45:51 +0530

    Create Filter_Program_OOP.java

---
+++ Filter_Program_OOP.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Container {
+    String[] array;
+    String startsWith;
+    public Container(String[] inputArray, String inputString){
+        array = inputArray;
+        startsWith = inputString;
+    }
+}
+class FilterArray {
+    public ArrayList filterMethod (String[] inputArray, String inputString){
+        ArrayList<String> filteredArray = new ArrayList<>();
+        for(int i=0;i<inputArray.length;i++){
+              if((inputArray[i]).startsWith(inputString)){
+                  filteredArray.add(inputArray[i]);
+              }
+        }
+        return filteredArray;
+    }
+}
+class DisplayFilteredArray {
+    public void displayInTerminal(ArrayList<String> list){
+        for(int i=0;i<list.size();i++)
+            System.out.println(list.get(i));
+    }
+}
+class FilterStringArray {
+    public static void main(String[] args) {
+        // Variable declaration and input
+        String[] inputArray = { "ask","play","type","code","asia","app" };
+        
+        ArrayList<String> filteredArray = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter the starting string to search: ");
+        String inputString = sc.nextLine();
+        
+        // Object creation
+        Container containerObj = new Container(inputArray,inputString);
+        FilterArray filterObj = new FilterArray();
+        DisplayFilteredArray displayObj = new DisplayFilteredArray();
+        
+        // Method invokation
+        filteredArray = filterObj.filterMethod(containerObj.array, inputString);
+        displayObj.displayInTerminal(filteredArray);
+    }
+}
