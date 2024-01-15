f81b362db07bc7087c091deb2de4f45e1a9dd64e
Joel Ralte <109339233+joel-ralte@users.noreply.github.com>
2024-01-15 19:01:18 +0530

    Update ThreadManager_Observer.java

--- ThreadManager_Observer.java
+++ ThreadManager_Observer.java
@@ -1,12 +1,9 @@
 import java.util.*;
 
 interface Subject {
-    public void subscribe(Observer o);
-    public void unsubscribe(Observer o);
-    private void notifyObservers() {};
-}
-interface Observer {
-    public void update(String state);
+    public void subscribeThread(Observer o);
+    public void unsubscribeThread(Observer o);
+    private void notifyThreadObservers() {};
 }
 class Thread implements Subject{
     int id;
@@ -18,44 +15,54 @@ class Thread implements Subject{
     public Thread() {
         state = "Created";
         observerList = new ArrayList<>();
+        notifyThreadObservers();
     }
     public void start() {
         state = "Running";
+        notifyThreadObservers();
     }
     public void abort() {
         state = "Aborted";
+        notifyThreadObservers();
     }
     public void sleep() {
         state = "Sleeping";
+        notifyThreadObservers();
     }
     public void waitThread() {
         state = "Waiting";
+        notifyThreadObservers();
     }
     public void suspend() {
         state = "Suspended";
+        notifyThreadObservers();
     }
     public void stop() {
         state = "Stopped";
+        notifyThreadObservers();
     }
-    public void subscribe(Observer o) {
+    public void subscribeThread(Observer o) {
         observerList.add(o);
     }
-    public void unsubscribe(Observer o) {
+    public void unsubscribeThread(Observer o) {
         observerList.remove(observerList.indexOf(o));
     }
-    private void notifyObservers() {
+    private void notifyThreadObservers() {
         for (Iterator<Observer> it =
               observerList.iterator(); it.hasNext();)
         {
             Observer o = it.next();
-            o.update(state);
+            o.updateState(state);
         }
     }
+}
+interface Observer {
+    public void updateState(String state);
 }
 class User implements Observer {
     private String state;
     
-    public void update(String state) {
+    public void updateState(String state) {
         this.state = state;
     }
     public void display() {
@@ -67,9 +74,15 @@ class ThreadManager {
         User user1 = new User();
         Thread thread = new Thread();
         
-        thread.subscribe(user1);
-        thread.notifyObservers();
+        thread.subscribeThread(user1);
+        
+        thread.start();
         user1.display();
-        thread.unsubscribe(user1);
+        thread.stop();
+        user1.display();
+        thread.abort();
+        user1.display();
+        
+        thread.unsubscribeThread(user1);
     }
 }
