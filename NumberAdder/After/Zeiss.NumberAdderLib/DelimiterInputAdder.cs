157cee60269440f8f41e62af5aaf6da0536abb24
Joel Ralte <109339233+joel-ralte@users.noreply.github.com>
2024-02-05 15:12:18 +0530

    Add files via upload

---
+++ NumberAdder/After/Zeiss.NumberAdderLib/DelimiterInputAdder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zeiss.NumberAdderLib
+{
+    internal class DelimiterInputAdder
+    {
+        public static int AddDelimiterInputNumbers(string input)
+        {
+            long sum = 0;
+            var delimiters = new char[] { ',', '\n' };
+            var numbers = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var number in numbers)
+            {
+                sum += long.Parse(number);
+                if (sum > int.MaxValue) { return int.MaxValue; }
+            }
+            return (int)sum;
+        }
+    }
+}
