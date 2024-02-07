e9090643dc982f4fa4a33ec0e8ef19f14a0b92d6
Joel Ralte <109339233+joel-ralte@users.noreply.github.com>
2024-02-07 13:49:54 +0530

    Add files via upload

---
+++ NumberAdder/After/Zeiss.NumberAdderUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Zeiss.NumberAdderUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Carl Zeiss AG")]
+[assembly: AssemblyProduct("Zeiss.NumberAdderUnitTest")]
+[assembly: AssemblyCopyright("Copyright © Carl Zeiss AG 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4d38ce7b-9f4c-44c8-92f9-b072a464a844")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
